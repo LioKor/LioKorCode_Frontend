f3d4bc306495de50350c7c355a731b5ad4cd23b5
Сергей <tyapkin2002@mail.ru>
2022-04-05 22:24:04 +0300

    Починил модалки

--- src/utils/Modal.vue
+++ src/utils/Modal.vue
@@ -37,12 +37,12 @@
 </style>
 <template>
   <div v-show="isShowed" class="modal-background">
-    <span class="close-btn modal-close" @click="__close(null)">
+    <span class="close-btn modal-close" @click="__resolve(null)">
       <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16"><path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"/></svg>
     </span>
 
-    <form class="standalone-form" novalidate @submit.prevent="__submit()">
-      <span class="close-btn" @click="__close(null)">
+    <form class="standalone-form" novalidate @submit.prevent="__resolve(type === 'prompt' ? text : true)">
+      <span class="close-btn" @click="__resolve(null)">
         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16"><path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"/></svg>
       </span>
 
@@ -59,8 +59,8 @@
         <div class="form-group">
           <input class="btn submit" v-if="type !== 'confirm'" type="submit" value="Ок">
           <div v-else class="confirm-buttons">
-            <button @click="__submit(true)" class="confirm-button btn submit" ref="buttonYes">Да</button>
-            <button @click="__submit(false)" class="confirm-button btn btn-danger">Нет</button>
+            <button @click="__resolve(true)" class="confirm-button btn submit" ref="buttonYes">Да</button>
+            <button @click="__resolve(false)" class="confirm-button btn btn-danger">Нет</button>
           </div>
         </div>
       </div>
@@ -80,39 +80,27 @@
     },
 
     methods: {
-      async __createModal(title, description = '', type='alert') {
-        this.isShowed = true;
+      __createModal(title, description = '', type='alert') {
         this.type = type;
         this.title = title;
         this.description = description;
 
+        if (this.isShowed)
+          return this.promise;
+        this.isShowed = true;
+
         const promise = new Promise((resolve) => {
           this.resolvePromise = resolve;
         });
-
-        if (this.isShowed)
-          return this.promise;
-
         this.promise = promise;
+
         return promise;
       },
-      __close(result) {
+      __resolve(result) {
         if (!this.isShowed)
           return;
 
         this.resolvePromise(result);
-        this.isShowed = false;
-        this.text = "";
-      },
-      __submit(result) {
-        if (!this.isShowed)
-          return;
-
-        if (this.type === 'prompt') {
-          this.resolvePromise(this.text);
-        } else {
-          this.resolvePromise(result);
-        }
         this.isShowed = false;
         this.text = "";
       },
