5d94d180d7715e17fc7a5ae4d6c1756a88c32a33
KoroLion <korolion31@yandex.ru>
2022-04-09 13:17:22 +0300

    unused elements commented out

--- src/apps/my tasks/Header.vue
+++ src/apps/my tasks/Header.vue
@@ -5,8 +5,8 @@
   <div class="header">
     <div class="topLine">
       <router-link to="/">Все задания</router-link>
-      <router-link to="#">Туда</router-link>
-      <router-link to="#">Сюда</router-link>
+<!--      <router-link to="#">Туда</router-link>-->
+<!--      <router-link to="#">Сюда</router-link>-->
 
       <router-link v-if="$store.state.user.isLogined" to="/profile" class="right">{{$store.state.user.username}}</router-link>
       <router-link v-else to="/signin" class="right">Войти</router-link>
--- src/apps/task previews/Header.vue
+++ src/apps/task previews/Header.vue
@@ -5,17 +5,17 @@
   <div class="header">
     <div class="topLine">
       <router-link to="/tasks/my">Мои задания</router-link>
-      <router-link to="#">Туда</router-link>
-      <router-link to="#">Сюда</router-link>
+<!--      <router-link to="#">Туда</router-link>-->
+<!--      <router-link to="#">Сюда</router-link>-->
 
       <router-link v-if="$store.state.user.isLogined" to="/profile" class="right">{{$store.state.user.username}}</router-link>
       <router-link v-else to="/signin" class="right">Войти</router-link>
     </div>
     <div class="sidePart">
-      <div>Выполнено: 1/5</div>
-      <div>Осталось: 10</div>
-      <div>Оценка: 4</div>
-      <div>Курс: 3/20</div>
+      <div>Work in progress...</div>
+      <div>Курс: 0 / 0</div>
+      <div>Выполнено: 0 / 0</div>
+      <div>Оценка: 0</div>
     </div>
   </div>
 </template>
