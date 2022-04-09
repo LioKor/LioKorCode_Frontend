836626c64d0857c8479ff329b8d2cd33ca84d7ae
KoroLion <korolion31@yandex.ru>
2022-04-09 11:13:51 +0300

    improvement: logo, signin and signup update

--- src/apps/SignIn.vue
+++ src/apps/SignIn.vue
@@ -11,14 +11,14 @@
           <div class="primary">Вход</div>
           <div class="secondary">Рады видеть вас снова!</div>
         </div>
-        <div class="form">
+        <div class="form" @keydown.enter.prevent="signIn">
           <form novalidate>
-            <div class="form-group">
-              <label>ЛОГИН ИЛИ EMAIL<span class="error-text"></span></label>
+            <div class="form-group" :class="{ error: errors.username }" @input="errors.username = ''">
+              <label>ЛОГИН<span class="error-text">{{ errors.username }}</span></label>
               <input v-model="username" type="text" class="form-control" required autocomplete="on">
             </div>
-            <div class="form-group">
-              <label>ПАРОЛЬ<span class="error-text"></span></label>
+            <div class="form-group" :class="{ error: errors.password }" @input="errors.password = ''">
+              <label>ПАРОЛЬ<span class="error-text">{{ errors.password }}</span></label>
               <input v-model="password" type="password" class="form-control" required autocomplete="on">
               <!-- <div class="muted"><linkButton href="#">Забыли пароль?</linkButton> -->
             </div>
@@ -43,18 +43,38 @@
       return {
         username: "",
         password: "",
+        errors: {}
       }
     },
 
     methods: {
       async signIn() {
+        this.errors = {}
+
+        if (this.username.length === 0) {
+          this.errors.username = 'Логин не может быть пустым!'
+          return;
+        } else if (this.password.length === 0) {
+          this.errors.password = 'Пароль не может быть пустым!'
+          return
+        }
+
         const response = await this.$store.state.api.signIn(this.username, this.password);
-        if (!response.ok_) {
-          this.$store.state.popups.error("Логин или пароль не подходят");
+        if (response.ok_) {
+          await this.$store.dispatch('GET_USER');
+          await this.$router.push('/profile');
+          this.$store.state.popups.success('Успешный вход!')
           return;
         }
-        await this.$store.dispatch('GET_USER');
-        this.$router.push('/profile');
+
+        const status = response.status_;
+        if (status === 403) {
+          const error = 'Неверный логин или пароль'
+          this.errors.username = error
+          this.errors.password = error
+        } else {
+          this.$store.state.popups.error('Не удалось войти!', 'Произошла непредвиденная ошибка!')
+        }
       }
     }
   }
--- src/apps/SignUp.vue
+++ src/apps/SignUp.vue
@@ -10,34 +10,39 @@
         <div class="title">
           <div class="primary">Регистрация</div>
         </div>
-        <div class="form">
+        <div class="form" @keydown.enter.prevent="signUp">
           <form novalidate>
-            <div class="form-group">
-              <label>ЛОГИН*<span class="error-text"></span></label>
+            <div class="form-group" :class="{ error: errors.username }" @input="errors.username = ''">
+              <label>ЛОГИН*<span class="error-text">{{ errors.username }}</span></label>
               <input v-model="username" type="text" class="form-control" required autocomplete="on">
               <div class="muted">Минимум 3 символа, только буквы, цифры и _</div>
             </div>
-            <div class="form-group">
-              <label>ПАРОЛЬ*<span class="error-text"></span></label>
+
+            <div class="form-group" :class="{ error: errors.email }" @input="errors.email = ''">
+              <label>EMAIL*<span class="error-text">{{ errors.email }}</span></label>
+              <input v-model="email" type="email" class="form-control" placeholder="wolf@liokor.ru" autocomplete="on">
+              <div class="muted">Используется для восстановления пароля, если не указан - восстановить пароль крайне сложно</div>
+            </div>
+
+            <div class="form-group" :class="{ error: errors.password }" @input="errors.password = ''">
+              <label>ПАРОЛЬ*<span class="error-text">{{ errors.password }}</span></label>
               <input v-model="password" type="password" class="form-control" required autocomplete="off">
               <div class="muted">В пароле должно быть много всяких символов, но его надо не забыть</div>
             </div>
-            <div class="form-group">
-              <label>ПОДТВЕРЖДЕНИЕ ПАРОЛЯ*<span class="error-text"></span></label>
+
+            <div class="form-group" :class="{ error: errors.passwordConfirm }" @input="errors.passwordConfirm = ''">
+              <label>ПОДТВЕРЖДЕНИЕ ПАРОЛЯ*<span class="error-text">{{ errors.passwordConfirm }}</span></label>
               <input v-model="passwordConfirm" type="password" class="form-control" required autocomplete="off">
             </div>
-            <div class="form-group">
-              <label>ПОЛНОЕ ИМЯ<span class="error-text"></span></label>
+
+            <div class="form-group" :class="{ error: errors.fullname }" @input="errors.fullname = ''">
+              <label>ПОЛНОЕ ИМЯ<span class="error-text">{{ errors.fullname }}</span></label>
               <input v-model="fullname" type="text" class="form-control" placeholder="Иван Иванов" autocomplete="on">
             </div>
+
             <div class="form-group">
-              <label>ЗАПАСНОЙ EMAIL<span class="error-text"></span></label>
-              <input v-model="email" type="email" class="form-control" placeholder="wolf@liokor.ru" autocomplete="on">
-              <div class="muted">Используется для восстановления пароля, если не указан - восстановить пароль крайне сложно</div>
-              <div class="form-group">
-                <div @click="signUp" class="btn">Создать</div>
-                <div class="muted">Уже с нами? <router-link to="/signin" class="router-link">Войти</router-link></div>
-              </div>
+              <div @click="signUp" class="btn">Создать</div>
+              <div class="muted">Уже с нами? <router-link to="/signin" class="router-link">Войти</router-link></div>
             </div>
           </form>
         </div>
@@ -59,25 +64,45 @@
         password: "",
         passwordConfirm: "",
         fullname: "",
+        errors: {}
       }
     },
 
     methods: {
       async signUp() {
-        //todo: username and email validations
+        this.errors = {}
+
+        if (this.username.length === 0) {
+          this.errors.username = 'Логин не может быть пустым'
+          return
+        }
+        if (this.password.length === 0) {
+          this.errors.password = 'Пароль не может быть пустым'
+          return
+        }
 
         if (this.password !== this.passwordConfirm) {
-          this.$store.state.popups.error("Пароли не совпадают");
+          this.errors.password = 'Пароли не совпадают'
+          this.errors.passwordConfirm = 'Пароли не совпадают'
           return;
         }
 
         const response = await this.$store.state.api.signUp(this.username, this.email, this.password, this.fullname);
-        if (!response.ok_) {
-          this.$store.state.popups.error("Не удалось создать пользователя");
+        if (response.ok_) {
+          this.$store.state.popups.success("Добро пожаловать!", `Пользователь ${this.username} успешно создан!`);
+          await this.$store.dispatch('GET_USER');
+          await this.$router.push('/profile');
           return;
         }
-        await this.$store.dispatch('GET_USER');
-        this.$router.push('/profile');
+
+        const status = response.status_
+
+        if (status === 409) {
+          this.errors.username = 'Такой логин уже занят('
+          this.errors.email = 'Или EMail уже занят... Мы точно не знаем)'
+        } else {
+          this.$store.state.popups.error("Не удалось создать пользователя", 'Произошла неизвестная ошибка!');
+        }
       }
     }
   }
