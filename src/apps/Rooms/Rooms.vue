cf3fffe37661f13f3b236af71b273e248a603153
KoroLion <korolion31@yandex.ru>
2022-04-12 01:24:29 +0300

    WIP: WebRTC

--- src/apps/Rooms/Rooms.vue
+++ src/apps/Rooms/Rooms.vue
@@ -1,6 +1,8 @@
 <style lang="stylus">
 @import '../../styles/constants.styl'
 
+width = 640px
+
 #rooms
   height 100vh
   background-color #0c2129
@@ -10,7 +12,7 @@
   padding 0 20px
 
   .room-join-or-create
-    max-width 500px
+    max-width width
 
     .create-room
       margin-bottom 10px
@@ -28,13 +30,18 @@
       background-color clHighlight
 
   .room-users
+    max-width width
     .user
       margin-bottom 10px
 </style>
 
 <template>
-  <div @contextmenu.prevent="state = !state">
-    <div v-if="!connected">
+  <div class="rooms">
+    <div v-if="!this.$store.state.user.isLogined">
+      <h1>Комнаты</h1>
+      <h2><router-link to="/signin">Авторизуйтесь</router-link>, чтобы создать или присоединиться к комнате</h2>
+    </div>
+    <div v-else-if="!connected">
       <h1>Комнаты</h1>
       <div v-if="wasConnected">
         <h2>Соединение с сервером разорвано.</h2>
@@ -45,10 +52,6 @@
         <h2>Пробуем еще{{ dots }}</h2>
       </div>
     </div>
-    <div v-else-if="!this.$store.state.user.isLogined">
-      <h1>Комнаты</h1>
-      <h2><router-link to="/signin">Авторизуйтесь</router-link>, чтобы создать или присоединиться к комнате</h2>
-    </div>
     <div v-else-if="!joinedRoom">
       <div class="room-join-or-create">
         <h1>Комнаты</h1>
@@ -56,7 +59,7 @@
         <h2>Создать</h2>
         <form>
           <div class="form-group">
-            <input v-model="createName" type="text" class="form-control" placeholder="Название">
+            <input v-model="createName" type="text" class="form-control" placeholder="Название" maxlength="48">
           </div>
 
 <!--          <div class="form-group">-->
@@ -82,7 +85,10 @@
     </div>
     <div v-else class="room-users">
       <h1>{{ joinedRoom.name }}</h1>
-      <button class="btn btn-danger" @click="roomLeave">Покинуть</button>
+      <button class="btn btn-danger" @click="roomLeave">
+        <span v-if="joinedRoom.host">Удалить</span>
+        <span v-else>Покинуть</span>
+      </button>
       <h2>Участники {{ joinedRoom.users.length }}<!-- / {{ joinedRoom.maxUsers }}--></h2>
       <div class="users">
         <div v-for="user in joinedRoom.users" class="user form-control">
@@ -100,7 +106,7 @@
 
 import Chat from './Chat.vue'
 
-import { Room, Message } from './models'
+import { Room, User, Message } from './models'
 
 const WS_ADDR = (window.location.hostname === 'localhost')? 'localhost:9090': `${window.location.hostname}/ws`
 const WS_ROOMS_URL = `${(window.location.protocol === 'http:')? 'ws': 'wss'}://${WS_ADDR}`
@@ -129,15 +135,159 @@ export default {
       createMaxUsers: 10,
 
       rooms: [],
+
+      iceServers: null,
+
+      devices: null,
+      stream: null
     }
   },
 
   methods: {
+    send(data) {
+      ws.send(JSON.stringify(data))
+    },
+
+    __getUser(id) {
+      for (const user in this.joinedRoom.users) {
+        if (user.id === id) {
+          return user
+        }
+      }
+      return null
+    },
+
+    async __getDevices() {
+      try {
+        /*if (!window.localStorage.getItem('hasMediaPermission')) {
+          this.$store.state.modal('Доступ к устройствам', 'Сейчас мы попросим предоставить доступ к вашему микрофону и камере.');
+        }*/
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: true
+        });
+
+        const devices = await navigator.mediaDevices.enumerateDevices();
+
+        for (const track of stream.getTracks()) {
+          track.stop();
+        }
+
+        // window.localStorage.setItem('hasMediaPermission', true);
+        return devices;
+      } catch (e) {
+        // window.localStorage.getItem('hasMediaPermission', false);
+        throw (e);
+      }
+    },
+
+    __createPeerConnection(user, stream = null) {
+      const pc = new RTCPeerConnection({
+        iceServers: this.iceServers
+      });
+
+      if (stream) {
+        for (const track of stream.getTracks()) {
+          pc.addTrack(track, stream);
+        }
+      }
+
+      pc.addEventListener('icecandidate', (ev) => {
+        if (!ev.candidate) {
+          return;
+        }
+
+        this.send({
+          to: user.id,
+          command: 'candidate',
+          candidate: ev.candidate
+        });
+      });
+
+      pc.addEventListener('iceconnectionstatechange', () => {
+        console.log(`RTC status changed for ${user.username}: ${pc.iceConnectionState}`);
+      });
+
+      pc.addEventListener('track', async (ev) => {
+        user.stream = ev.streams[0];
+        // this.usersView.render(this.users);
+      });
+
+      pc.addEventListener('datachannel', (ev) => {
+        console.log('Data channel created!')
+        ev.channel.addEventListener('message', (ev) => {
+          console.log(ev.data)
+          // addMessage(messagesDiv, {
+          //   username: user.username,
+          //   avatarUrl: user.avatarUrl,
+          //   datetime: new Date(),
+          //   content: ev.data
+          // })
+          // this.messageSound.play()
+        })
+      })
+
+      return pc;
+    },
+
+    async __offerReceived(offer, from) {
+      const user = this.__getUser(from);
+      if (!user) {
+        console.log(`ERROR: offer, unable to find user with id = ${from}`);
+        return;
+      }
+      console.log(`RTC offer received from ${user.username}`);
+
+      user.pc = await this.__createPeerConnection(user);
+
+      await user.pc.setRemoteDescription(offer);
+      const answer = await user.pc.createAnswer();
+      await user.pc.setLocalDescription(answer);
+      this.send({
+        to: user.id,
+        command: 'answer',
+        data: answer
+      });
+    },
+
+    async __answerReceived(answer, from) {
+      const user = this.__getUser(from);
+
+      if (!user) {
+        console.log(`ERROR: answer, unable to find user with id = ${from}`);
+        return;
+      }
+
+      console.log(`RTC answer received from ${user.username}`);
+
+      if (!user.pc) {
+        console.log(`ERROR: ${user.username} does not have peer connection!`);
+        return;
+      }
+
+      await user.pc.setRemoteDescription(answer);
+    },
+
+    __candidateReceived(candidate, from) {
+      const user = this.__getUser(from);
+      if (!user) {
+        console.log(`ERROR: candidate, unable to find user with id = ${from}`);
+        return;
+      }
+      console.log(`RTC candidate received from ${user.username}: ${candidate.candidate}`);
+
+      try {
+        user.pc.addIceCandidate(candidate);
+      } catch (e) {
+        console.log(`Unable to add candidate from ${user.username}`);
+      }
+    },
+
     sendMessage(message) {
-      ws.send(JSON.stringify({
+      this.send({
         command: 'sendMessage',
         content: message
-      }));
+      })
     },
 
     updateDots() {
@@ -149,25 +299,46 @@ export default {
     },
 
     roomJoin(id) {
-      ws.send(JSON.stringify({
+      this.send({
         command: 'joinRoom',
         id: id
-      }));
+      })
     },
 
-    roomCreate() {
-      const data = JSON.stringify({
+    async roomCreate() {
+      if (this.createName.length === 0) {
+        this.$store.state.modal.alert('Название не может быть пустым')
+        return
+      }
+
+      this.devices = await this.__getDevices()
+
+      this.stream = await navigator.mediaDevices.getUserMedia({
+        audio: {
+          echoCancellation: true,
+          noiseSuppression: true
+        },
+        video: true
+      });
+
+      const data = {
         command: 'createRoom',
         name: this.createName,
         maxUsers: this.createMaxUsers
-      })
-      ws.send(data);
+      }
+      this.send(data);
     },
 
     roomLeave() {
-      ws.send(JSON.stringify({
+      if (this.stream) {
+        for (const track of this.stream.getTracks()) {
+          track.stop()
+        }
+      }
+
+      this.send({
         command: 'leaveRoom',
-      }))
+      })
     },
 
     async waitForUsername() {
@@ -178,8 +349,23 @@ export default {
             clearInterval(checkInterval);
             resolve()
           }
-        }, 50)
+        }, 100)
       })
+    },
+
+    async userConnected(user) {
+      user.pc = this.__createPeerConnection(user, this.stream)
+      const offer = await user.pc.createOffer({
+        offerToReceiveAudio: 1
+      })
+      await user.pc.setLocalDescription(offer)
+      this.send({
+        to: user.id,
+        command: 'offer',
+        offer: offer
+      })
+
+      this.joinedRoom.addUser(user)
     },
 
     createWsConnection() {
@@ -193,8 +379,12 @@ export default {
 
       ws.addEventListener('close', function() {
         this.connected = false
+        this.joinedRoom = null
         ws = null
         setTimeout(function() {
+          if (this.connected) {
+            return
+          }
           this.updateDots()
           this.createWsConnection()
           if (reconnectTimeout < MAX_RECONNECT_TIMEOUT) {
@@ -208,11 +398,11 @@ export default {
         this.connected = true
         this.wasConnected = true
 
-        ws.send(JSON.stringify({
+        this.send({
           command: 'setInfo',
           username: this.$store.state.user.username
-        }))
-        ws.send(JSON.stringify({ command: 'getRooms' }));
+        })
+        this.send({ command: 'getRooms' })
       })
 
       ws.addEventListener('message', (message) => {
@@ -221,7 +411,15 @@ export default {
         if (data.command === 'setRooms') {
           this.rooms = data.rooms
         } else if (data.command === 'setRoom') {
-          this.joinedRoom = new Room(data.id, data.name, data.maxUsers, data.users)
+          if (this.joinedRoom) {
+            return
+          }
+          const users = []
+          for (const user in data.users) {
+            users.push(new User(user.id, user.username))
+          }
+          const host = data.users[0].username === this.$store.state.user.username;
+          this.joinedRoom = new Room(data.id, data.name, data.maxUsers, host, data.users)
         } else if (data.command === 'leaveRoom') {
           if (data.kick) {
             alert('Вы были исключены из комнаты!')
@@ -233,8 +431,22 @@ export default {
           }
           const message = new Message(data.username, data.content)
           this.$refs.chat.addMessage(message)
+        } else if (data.command === 'addRoomUser') {
+          this.userConnected(new User(data.id, data.username))
+        } else if (data.command === 'deleteRoomUser') {
+          this.joinedRoom.deleteUser(data.id)
+        } else if (data.command === 'setInfo') {
+          this.iceServers = data.iceServers
+        } else if (data.command === 'candidate') {
+          this.__candidateReceived(data.candidate, data.from)
+        } else if (data.command === 'offer') {
+          this.__offerReceived(data.offer, data.from)
+        } else if (data.command === 'answer') {
+          this.__answerReceived(data.answer, data.from)
         } else if (data.command === 'error') {
           console.log(`WS ERROR: ${data.message}`);
+        } else {
+          console.log(`WS ERROR: Unknown command ${data.command} received`)
         }
       });
     }
--- src/apps/Rooms/models.js
+++ src/apps/Rooms/models.js
@@ -7,12 +7,41 @@ export class Message {
     }
 }
 
+export class User {
+    constructor(id, username) {
+        this.id = id
+        this.username = username
+
+        this.stream = null
+        this.pc = null
+        this.dc = null
+    }
+}
+
 export class Room {
-    constructor(id, name, maxUsers, users) {
+    constructor(id, name, maxUsers, host, users) {
         this.id = id
         this.name = name;
         this.maxUsers = maxUsers;
+        this.host = host;
 
         this.users = users
+    }
+
+    addUser(user) {
+        this.users.push(user)
+    }
+
+    deleteUser(id) {
+        console.log(id)
+        console.log(this.users)
+        for (let i = 0; i < this.users.length; i++) {
+            const user = this.users[i];
+
+            if (user.id === id) {
+                this.users.splice(i, 1)
+                return
+            }
+        }
     }
 }
