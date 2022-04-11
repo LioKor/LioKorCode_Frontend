20ef733ef0df7a2d33de25d0113576cd36d48210
Сергей <tyapkin2002@mail.ru>
2022-04-11 18:00:07 +0300

    fix merging modals

--- src/utils/Modal.vue
+++ src/utils/Modal.vue
@@ -46,10 +46,9 @@
         text-shadow textLightingNormal1
 </style>
 <template>
-  <div v-show="isShowed" class="modal-background">
-    <span class="close-btn modal-close" @click="__resolve(null)">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16"><path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"/></svg>
-    </span>
+  <div class="modal" v-show="isShowed" @keydown.enter.prevent="__resolve(true)" @keydown.esc="__resolve(false)">
+    <div class="modal-background" @click="__resolve(null)">
+    </div>
 
     <div class="standalone-form" ref="form">
       <span class="close-btn" @click="__resolve(null)">
