39d62596dc066a9106b866cd886bc125d9ef8727
KoroLion <korolion31@yandex.ru>
2022-04-24 15:29:36 +0300

    config: devLocal command to proxy to local API

--- webpack.config.js
+++ webpack.config.js
@@ -5,92 +5,96 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-            favicon: 'src/images/favicon.ico'
-        }),
-        new VueLoaderPlugin(),
-        new webpack.DefinePlugin({
-            BUILD_TIMESTAMP: Date.now(),
-            VERSION: JSON.stringify(require('./package.json').version)
-        })
-    ],
-    devServer: {
-        port: 9000,
-        historyApiFallback: {
-            rewrites: [
-                {
-                    from: /.(js|png|ico|mp3)$/,
-                    to: (context) => {
-                        const path = context.parsedUrl.pathname.split('/')
-                        return `/${path[path.length - 1]}`
-                    }
-                },
-                { from: /^\/#/, to: '/index.html' },
-            ]
+module.exports = env => {
+    const proxyTo = (env.local)? 'http://localhost:9091': 'https://code.liokor.com/'
+
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/'
         },
-        proxy: {
-            '/api': {
-                target: 'https://code.liokor.com',
-                secure: false,
-                changeOrigin: true
-            }
-        }
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-    },
-    module: {
-        rules: [
-            {
-              test: /\.vue$/,
-              use: [
-                  'vue-loader',
-              ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ],
-            },
-            {
-                test: /\.png$/,
-                loader: 'file-loader'
-            },
-            {
-                test: /\.mp3$/,
-                loader: 'file-loader'
-            },
-            {
-                test: /\.worker.js$/,
-                use: { loader: 'worker-loader' }
-            },
-            {
-                test: /\.styl(us)?$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'stylus-loader'
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'src/index.html',
+                favicon: 'src/images/favicon.ico'
+            }),
+            new VueLoaderPlugin(),
+            new webpack.DefinePlugin({
+                BUILD_TIMESTAMP: Date.now(),
+                VERSION: JSON.stringify(require('./package.json').version)
+            })
+        ],
+        devServer: {
+            port: 9000,
+            historyApiFallback: {
+                rewrites: [
+                    {
+                        from: /.(js|png|ico|mp3)$/,
+                        to: (context) => {
+                            const path = context.parsedUrl.pathname.split('/')
+                            return `/${path[path.length - 1]}`
+                        }
+                    },
+                    { from: /^\/#/, to: '/index.html' },
                 ]
             },
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-            },
-            {
-                test :/\.exec\.js$/,
-                use: 'script-loader'
+            proxy: {
+                '/api/v1': {
+                    target: proxyTo,
+                    secure: false,
+                    changeOrigin: true
+                }
             }
-        ],
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/,
+                    use: [
+                        'vue-loader',
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                    ],
+                },
+                {
+                    test: /\.png$/,
+                    loader: 'file-loader'
+                },
+                {
+                    test: /\.mp3$/,
+                    loader: 'file-loader'
+                },
+                {
+                    test: /\.worker.js$/,
+                    use: { loader: 'worker-loader' }
+                },
+                {
+                    test: /\.styl(us)?$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'stylus-loader'
+                    ]
+                },
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                },
+                {
+                    test :/\.exec\.js$/,
+                    use: 'script-loader'
+                }
+            ],
+        }
     }
 };
